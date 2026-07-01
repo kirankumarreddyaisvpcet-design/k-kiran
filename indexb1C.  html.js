226cb5357ec82b2245606fce6de98c81267437a3
kirankumarreddyaisvpcet-design <kirankumarreddyai.svpcet@gmail.com>
2026-07-01 14:16:46 +0530

    Add files via upload

---
+++ indexb1C.  html.js
@@ -0,0 +1,16 @@
+ <script>
+        document.addEventListener("DOMContentLoaded", function () {
+
+            let button = document.getElementById("myButton");
+
+            button.addEventListener("click", function () {
+
+                let text = document.getElementById("text");
+
+                text.textContent = "The text has been changed!";
+                text.style.color = "green";
+
+            });
+
+        });
+    </script>
